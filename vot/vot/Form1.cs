90f75af0f71fd659a1ae61176c1137711bc3fc33
ajulyav <ajulyav@mail.ru>
2016-11-10 11:31:06 +0300

    Added label1 and action button1_Click

--- vot/vot/Form1.cs
+++ vot/vot/Form1.cs
@@ -14,5 +14,10 @@ namespace vot
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "It work!!!";
+        }
     }
 }
